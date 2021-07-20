b9c71bb10199f1b8377e1726e198a9ba871af512
Anri <anrihunanyan@gmail.com>
2021-07-20 17:38:45 +0400

    feat(Input.jsx): created input component

---
+++ src/Components/Input/Input.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import classNames from "classnames";
+
+const style = classNames(
+  "outline-none",
+  "p-3",
+  "text-red-600",
+  "max-w-lg"
+);
+
+export default class Input extends React.Component {
+  state = {
+    value: "",
+  };
+
+  handleChange = ({ target }) => {
+    // e.stopPropagation();
+    this.setState({ value: target.value });
+    this.props.onChange(target.value);
+  };
+
+
+  render() {
+    const { type, name, placeholder, onClick, value, onChange } = this.props;
+    // const { value: stateValue } = this.state;
+    return (
+      <input
+        className={style}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        onClick={onClick}
+        value={value}
+        onChange={onChange}
+      />
+    );
+  }
+}
