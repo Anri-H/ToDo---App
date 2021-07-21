ca24c073b769e72225db21e2cbc9e39e928e9912
Anri <anrihunanyan@gmail.com>
2021-07-21 17:04:32 +0400

    feat(todo.jsx): created functions for todo items

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "../Button/Button";
 import Input from "../Input/Input";
 import classNames from "classnames";
+import List from "../List/List";
 const titleCSS = classNames("text-red-600", "text-5xl")
 
 export default class Todo extends React.Component {
@@ -11,11 +12,40 @@ export default class Todo extends React.Component {
     validationMessage: "",
   };
 
+  handleDelete = (val) => (e)=> {
+    e.stopPropagation()
+    console.log("delete");
+    this.setState(({todos})=>({todos: todos.filter(({id}) => id !== val)}))
+  };
+
+  handleEdit = (id) => (e)=>{
+    e.stopPropagation();
+    this.setState(({todos})=>({
+      todos: todos.map(item=> item.id===id ? {...item, isEdit: !item.isEdit} : item
+      )
+    }))
+  }
+
+  handleSaveEdit = (id) => (text)=>{
+    this.setState(({todos})=>({
+      todos: todos.map(item=> item.id===id ? {...item, text: text} : item
+      )
+    }))
+  }
+
+  handleComplete = (id)=>(e)=>{
+    e.stopPropagation()
+    this.setState(({todos})=>({
+      todos: todos.map(item=> item.id===id ? {...item, isComplete: !item.isComplete} : item
+      )
+    }))
+  }
+
   handleInputChange = ({target}) => {
     this.setState({ todoInput: target.value });
   };
   
-  
+
   handleAddTodo = ()=>{
     this.setState(({ todos, todoInput }) => ({
       todos: [...todos, todoItem(todoInput)],
@@ -24,6 +54,7 @@ export default class Todo extends React.Component {
   }
 
   render() {
+    const {todoInput, todos} = this.state;
     return (
       <>
         <h1 className={titleCSS}>ToDo</h1>
@@ -31,18 +62,20 @@ export default class Todo extends React.Component {
         <Input
           type="text"
           placeholder="Enter text"
+          value={todoInput}
           onChange={this.handleInputChange}
         />
-        <Button text="Add" disabled={!this.state.todoInput} onClick={this.handleAddTodo}/>
+        <Button text="Add" disabled={!todoInput} onClick={this.handleAddTodo}/>
         </div>
-        {this.state.todos}
+        <List todos={todos} complete={this.handleComplete} edit={this.handleEdit} saveEdit={this.handleSaveEdit}  deleteItem={this.handleDelete}/>
       </>
     );
   }
 }
 
 
-
+let id = 0;
 function todoItem(text){
-  return <p>{text}</p>
+  return {text: text,
+  id: id++}
 }
